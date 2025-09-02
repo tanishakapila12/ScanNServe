b9d447ef7d34479f7b4adf2d00ef2108944397f1
14Memansalman <stud.2201201080@kpgu.ac.in>
2025-09-02 17:54:01 +0530

    Complete fetching food data on the frontend from the database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express  from "express"
 import cors from 'cors'
-import { connectDB } from "./config/db.js";
+import { connectDB } from "./config/db.js"
 import 'dotenv/config'
 import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
 
 
 // app config
@@ -19,6 +20,7 @@ connectDB()
 // api endpoints
 app.use("/api/food", foodRouter)
 app.use("/images",express.static('uploads'))
+app.use("/api/user", userRouter)
 
 
 app.get("/", (req, res) => {

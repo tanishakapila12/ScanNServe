c4e6461341dbeb947c3c4b3078a73fa08faf1edb
kapilatanisha12 <kapilatanisha@gmail.com>
2025-09-08 20:14:37 +0530

    update 3

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -78,4 +78,19 @@ const verifyOrder = async (req, res) => {
 
 }
 
-export {placeOrder, verifyOrder}
+//user orders for frontend
+const userOrders = async (req, res) => {
+    try{
+        //to get all orders of a user
+        const orders = await orderModel.find({userId:req.body.userId})
+        res.json({success:true, data:orders} )
+
+    }
+    catch(error){
+        console.log(error);
+        res.json({success:false, message:"Error"})
+
+    }
+
+}
+export {placeOrder, verifyOrder, userOrders}
